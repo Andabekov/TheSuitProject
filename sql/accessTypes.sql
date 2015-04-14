a8f5c584b6e74b9a5b9c96a16090549b99607663
andabekov <andabekov1@gmail.com>
2015-04-14 10:20:14 +0600

    all menus are moved to the layout page. sql statements are updated

---
+++ sql/accessTypes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE 'accesstypes' (
+  'ID' int(11) NOT NULL,
+  'ACCESS_TYPE_NAME' varchar(45) NOT NULL,
+  PRIMARY KEY ('ID')
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO 'accesstypes' ('ID','ACCESS_TYPE_NAME') VALUES (1,'seller');
+INSERT INTO 'accesstypes' ('ID','ACCESS_TYPE_NAME') VALUES (2,'redactor');
+INSERT INTO 'accesstypes' ('ID','ACCESS_TYPE_NAME') VALUES (3,'accountant');
+INSERT INTO 'accesstypes' ('ID','ACCESS_TYPE_NAME') VALUES (4,'director');
+INSERT INTO 'accesstypes' ('ID','ACCESS_TYPE_NAME') VALUES (5,'delivery');
+INSERT INTO 'accesstypes' ('ID','ACCESS_TYPE_NAME') VALUES (6,'admin');
