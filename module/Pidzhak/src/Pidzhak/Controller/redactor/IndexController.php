62455f676a1ee8750a33386be4adc3aca544b36a
Yerganat Merzentay <yerganat.merzentay@bas.kz>
2015-04-27 00:15:35 +0600

    excel uploader

--- module/Pidzhak/src/Pidzhak/Controller/redactor/IndexController.php
+++ module/Pidzhak/src/Pidzhak/Controller/redactor/IndexController.php
@@ -9,6 +9,7 @@
 
 namespace Pidzhak\Controller\redactor;
 
+use Pidzhak\Form\Redactor\UploadForm;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
@@ -22,6 +23,32 @@ class IndexController extends AbstractActionController
 
         $view = new ViewModel(array('info' => $this->getServiceLocator()->get('AuthService')->getStorage()));
         $view->setTemplate('pidzhak/redactor/index.phtml');
+        return $view;
+    }
+
+    public function compareAction(){
+        $form = new UploadForm('upload-form');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            // Make certain to merge the files info!
+            $post = array_merge_recursive(
+                $request->getPost()->toArray(),
+                $request->getFiles()->toArray()
+            );
+
+            $form->setData($post);
+            if ($form->isValid()) {
+                $data = $form->getData();
+//                var_dump("<br>");
+                //var_dump($data);
+                // Form is valid, save the form!
+                //return $this->redirect()->toRoute('re');
+            }
+        }
+
+        $view = new ViewModel(array('form' => $form));
+        $view->setTemplate('pidzhak/redactor/upload-form.phtml');
         return $view;
     }
 }
